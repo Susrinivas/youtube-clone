8659c3dda71a43a440e0b58259f1edfdd5765bc7
Susrinivas <79036336+Susrinivas@users.noreply.github.com>
2021-06-07 22:50:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const Client_id = '999917681683-gdrcgkriod2gp5v0og1cshhim141tdov.apps.googleusercontent.com';
+const Client_id = '267085355717-egmkf1o2q9oocudbbtj3sr8gaqero5q4.apps.googleusercontent.com';
 const Discover_docs = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'];
 const Scopes = 'https://www.googleapis.com/auth/youtube';
-const Api_key = 'AIzaSyDgxU13QcmLImQNFFW-jPkdj9pAtrebvEc';
+const Api_key = 'AIzaSyDrjcA6BtE5-wEoOp0jTXGG0Gnkgj7tLlw';
 const authorizeButton = document.getElementById('authorize-button');
 const signoutButton = document.getElementById('signout-button');
+
 
 const channelform = document.getElementById('channel-form');
 const channeldata = document.getElementById('channel-data');
 const videos = document.getElementById('video-container');
 
-let defaultChannel = 'UC29ju8bIPH5as8OGnQzwJyA';
+let defaultChannel = 'UC_x5XG1OV2P6uZZ5FSM9Ttw';
 
 channelform.addEventListener('submit', e => {
     e.preventDefault();
     const channel1 = document.getElementById('channel_input').value;
-    console.log(channel1);
+    
     //getChannel(channel1);
     topicBasedSearching(channel1);
 })
@@ -22,13 +23,13 @@ channelform.addEventListener('submit', e => {
 const home = document.getElementById('home')
 home.addEventListener('click', () => {
     const channel1 = document.getElementById('channel_input').value;
-    console.log(channel1);
+    
     
     channeldata.innerHTML = '';
    if(!channel1){
         getChannel(defaultChannel);
     }else {
-      topicBasedSearching(channel1);
+        topicBasedSearching(channel1);
     }
 })
 //Load Auth2 library
@@ -48,7 +49,7 @@ function initClient(){
     }).then(() => {
         // Listen for sign in state changes
         gapi.client.setApiKey(Api_key)
-        console.log('initclient')
+        
         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus)
         // Handle intial sign in state
 
@@ -61,13 +62,15 @@ function initClient(){
 // Update UI sign in state changes
 
 function updateSigninStatus(isSignedIn){
-    console.log('updatestatus')
-    console.log("1")
-    console.log(isSignedIn)
+    
     if(isSignedIn) {
-      getChannel(defaultChannel)
+        signoutButton.style.display="block";
+        authorizeButton.style.display="none";
+        getChannel(defaultChannel);
     } else {
-        console.log('2')
+       
+        authorizeButton.style.display="block";
+        signoutButton.style.display="none";
         videos.style.display = 'none';
         channeldata.style.display='none';
     }
@@ -76,57 +79,48 @@ function updateSigninStatus(isSignedIn){
 // Handle login 
 let login = document.getElementsByClassName('login')[0];
 let logout = document.getElementsByClassName('logout')[0];
-
 function handleAuthClick(){
-    console.log('authclick')
+    
     gapi.auth2.getAuthInstance().signIn();
-    console.log(gapi)
-    login.style.display='none'
-    logout.style.display='block';
+    
+    login.style.color='red'
+    logout.style.color='black';
 }
 
 // Handle logout 
 function handleSignoutClick() {
     gapi.auth2.getAuthInstance().signOut()
-    login.style.display='block';
-    logout.style.display='none';
+    login.style.color='black';
+    logout.style.color='red';
 }
 
 // Get channel from  API 
  var channel = '';
+
+
+
+ // functionality to Retrieve channel information
+
 function getChannel(channelt){
    gapi.client.youtube.channels.list({
        part: 'snippet,contentDetails,statistics',
        //forUsername: channelt,
        id: channelt
       }).then(response => {
-       console.log(response);
-        channel= response.result.items[0];
+       channel= response.result.items[0];
         if(channelt == defaultChannel ){
         showVideos(channel);
         }
-      }).catch(err => alert('No channel By that name ..'))
+      })
+   .catch(err => alert('No channel By that name ..'))
 }
 
-// function getChannelfromSearch(channelt){
-//     gapi.client.youtube.channels.list({
-//         part: 'snippet,contentDetails,statistics',
-//         //forUsername: channelt,
-//         id: channelt
-//        }).then(response => {
-//         //console.log(response);
-//          channel= response.result.items[0];
-//          console.log(response);
-//          return response;
-//        })
-//     .catch(err => alert('No channel By that name seatrch'))
-//  }
- 
+// functionality to Retrieve Subscriptions and user activity.
+
 
 function retriveInfo() {
     videoContainer.innerHTML = '';
-    console.log(channel)
-  const output = `<ul class='collections'>
+   const output = `<ul class='collections'>
   <li class='collection-item'><span class='text-style'>Title: </span> ${channel.snippet.title}</li>
   <li class='collection-item'><span class='text-style'>Channel ID:</span> ${channel.id}</li>
   <li class='collection-item'><span class='text-style'>Channel Subscriber Count: </span> ${channel.statistics.subscriberCount}</li>
@@ -144,6 +138,10 @@ function retriveInfo() {
 
 const videoContainer = document.getElementById('video-container')
 
+// functionality to Uploaded videos and system-generated playlists
+
+
+
 function showVideos(channel){
     
     const requestoptions = {
@@ -151,26 +149,23 @@ function showVideos(channel){
         playlistId: channel.contentDetails.relatedPlaylists.uploads,
         maxResults: 16
     }
-   console.log('data');
+   
     const request = gapi.client.youtube.playlistItems.list(requestoptions);
     
     request.execute(response => {
 
-        console.log(response);
+       
         const playlistitems = response.result.items;
         if(playlistitems){
             let  res = "<h4 class='center-align'></h4>";
             playlistitems.forEach(item => {
-           let VideoId = item.snippet.resourceId.videoId;
+           const videoId = item.snippet.resourceId.videoId;
 
            res +=  `
-                   <div class='col-3 history' id=${VideoId} >
-                   <iframe  width="100%" height="250" src="https://www.youtube.com/embed/${VideoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                   <div4 class='col-3 history' id=${videoId}>
+                   <iframe class='myframe' width="100%" height="250" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                    <p>${channel.snippet.title}</p>
-                   <button onclick='updatePlaylist(${channel.snippet.title}, ${channel.snippet.description}, ${channel.contentDetails.relatedPlaylists.uploads})'>Add to playlist</button>
-                   <hr>
-                   <br>
-                   </div>
+                   </div4>
            ` ;
          })
          
@@ -183,19 +178,9 @@ function showVideos(channel){
 
 }
 
-    // function topicBasedSearching(channel){
-    //     console.log('topic')
-    //     gapi.client.youtube.search.list({
-    //         part: 'snippet,contentDetails,statistics',
-    //         topicId: '/m/01h6rj',
-    //     }).then(response => {
-    //         console.log(response);
-    //          channel= response.result.items[0];
-    //          Topic(response)
-             
-    //        })
-    //     .catch(err => alert('No channel By that name'))
-    //  }
+    //Create functionality for topic based searching and search for playlists or channels
+
+
     
      
      function topicBasedSearching(resp){
@@ -204,224 +189,99 @@ function showVideos(channel){
             q: `${resp}`,
            maxResults: 16,
            }).then(response => {
-                    console.log(response);
                     Topic(response)
          })
                 .catch(err => alert('No channel By that name yes'))
         }
 
-      var playlisttitle = '';
-      var plalistdescription = '';
+
         function Topic(resp){
        
-            console.log(resp);
+            
             const listitems = resp.result.items;
             if(listitems){
-                let  r = "<h4 class='center-align'></h4>";
+                let  res = "<h4 class='center-align'></h4>";
                 listitems.forEach(item => {
-               let videoid = item.id.videoId;
-               
-               r +=  `
-                       <div class='col-3 history' id=${videoid} >
-                       <iframe  width="100%" height="250" src="https://www.youtube.com/embed/${videoid}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                       <object class='fit' data=${item.snippet.thumbnails.default.url}>
+               const videoid = item.id.videoId;
+               res +=  `
+                       <div4 class='col-3'   id=${videoid}>
+                       <iframe  class='myframe' width="100%" height="250" src="https://www.youtube.com/embed/${videoid}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                      <object class='fit' data=${item.snippet.thumbnails.default.url}>
                        </object>
                        <p>${item.snippet.title}</p>
                        <p style='display:inline;'>${item.snippet.channelTitle} </p>
-                       <button onclick='updatePlaylist(${item.snippet.title}, ${item.snippet.description}, ${videoid}, ${item.snippet.playlistId})'>Add to playlist</button>
-                       <hr>
-                       <br>
-                       </div>
-                       
-                     
+                       </div4>
                ` ;
-               
-                     getChannel(item.snippet.channelId);
+                 getChannel(item.snippet.channelId);
                
                 })
              
-             videoContainer.innerHTML = requestAnimationFrame;
+             videoContainer.innerHTML = res;
             }else{
-               // videoContainer.innerHTML = 'No Uploaded Videos';
+                videoContainer.innerHTML = 'No Uploaded Videos';
                 
             }
+             
             
         }
-        var histvideos = '';
 
-        let hist = document.getElementsByClassName('history');
-      //let j=0;
-
-        for(var i=0; i<hist.length; i++){
-          //console.log('event',j++)
-         hist[i].addEventListener('click', event => {
-         
-               let video_id = event.target.id;
-               histvideos +=  `
-                       <div class='col-3'>
-                       <iframe  width="100%" height="250" src="https://www.youtube.com/embed/${video_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                       <hr>
-                       <br>
-                       </div> ` ;
-                      
-
-          } )
-        }
-      
-       
         
-        // function history(video){
-        //   console.log('histvideos')   
-        //   histvideos +=  `
-        //                <div class='col-3'>
-        //                <iframe  width="100%" height="250" src="https://www.youtube.com/embed/${video.id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        //  functionality to create and update a playlist. 
+
+
+
+
+        function playlist(){
+            document.getElementById('channel-data').innerHTML = '';
+         
+            let content = `<form id='create_playlist' onsubmit=formsubmit(event)>
+            <input type='text' class='form-control title mb-3 mx-auto'>
+            <input type='text' class='form-control description mb-3 mx-auto'>
+            <button type='submit' class='btn btn-primary'>Submit</button>
+            </form>`
+            videoContainer.innerHTML = content;
+             
+          }
+           
+          
+          var form1 = document.getElementById('create_playlist');
+        
+         function formsubmit(e) {
+            e.preventDefault();
+            let titleVal = document.getElementsByClassName('title')[0].value;
+            let descript = document.getElementsByClassName('description')[0].value;
+           
+            Createplaylist(titleVal,descript);
+          }
+        
+        
+          function Createplaylist(title,description){
+                 
+                   
+                     gapi.client.youtube.playlists.insert({
+                          'part' : 'snippet,status',
+                          "resource": {
+                            "snippet": {
+                              "title": `${title}`,
+                              "description": `${description}`,
+                              "tags": [
+                                "sample playlist",
+                                "API call"
+                              ],
+                              "defaultLanguage": "en"
+                            },
+                            "status": {
+                              "privacyStatus": "private"
+                            }
+                          }
                        
-        //                <hr>
-        //                <br>
-        //                </div>
-                     
-        //        ` ;
-
-         
-
-        // }
-
-        function displayHistory(){
-          console.log(histvideos)
-          videoContainer.innerHTML = histvideos;
-          console.log('displayh')
-        }
-
-        //playlist creation & updation
-  //var myplaylistId ='';
-  
-  function playlist(){
-    document.getElementById('channel-data').innerHTML = '';
-
-    let content = `<form id='create_playlist' onsubmit=formsubmit(event)>
-    <input type='text' class='form-control title mb-3 mx-auto'>
-    <input type='text' class='form-control description mb-3 mx-auto'>
-    <button type='submit' class='btn btn-primary'>Submit</button>
-    </form>`
-    videoContainer.innerHTML = content;
-     
-  }
-   
-  
-  var form1 = document.getElementById('create_playlist');
-
- function formsubmit(e) {
-    e.preventDefault();
-    let titleVal = document.getElementsByClassName('title')[0].value;
-    let descript = document.getElementsByClassName('description')[0].value;
-    console.log(titleVal,descript);
-    Createplaylist(titleVal,descript);
-  }
-
-
-  function Createplaylist(title,description){
-         
-            console.log('playlist')
-             gapi.client.youtube.playlists.insert({
-                  'part' : 'snippet,status',
-                  "resource": {
-                    "snippet": {
-                      "title": `${title}`,
-                      "description": `${description}`,
-                      "tags": [
-                        "sample playlist",
-                        "API call"
-                      ],
-                      "defaultLanguage": "en"
-                    },
-                    "status": {
-                      "privacyStatus": "private"
+                        }).then(response => {
+                            console.log(response);
+                            myplaylistId = response.result.snippet.channelId;
+                            console.log(myplaylistId);
+                        }).catch(err => {
+                            console.log('error')
+                        })
                     }
-                  }
-               
-                }).then(response => {
-                    console.log(response);
-                    myplaylistId = response.result.id;
-                    console.log(myplaylistId);
-                }).catch(err => {
-                    console.log('error')
-                })
-            }
-
-            function updatePlaylist(title, description,  myplaylistid) {
-                gapi.client.youtube.playlists.update({
-                  "part": [
-                    "snippet,status"
-                  ],
-                  "resource": {
-                    "id": `${myplaylistid}`,
-                    "snippet": {
-                      "title": `${title}`,
-                      "description": `${description}`,
-                      "tags": [
-                        "updated playlist",
-                        "API FTW"
-                      ]
-                    },
-                    "status": {
-                      "privacyStatus": "private"
-                    }
-                  }
-                }).then(response => {
-                    console.log(response)
-                }).catch(err => {
-                    console.log('error')
-                })
-            }
-
-
-            function addVideotoPlaylist() {
-                gapi.client.youtube.playlistItems({
-                  "part": [
-                    "snippet,status"
-                  ],
-                  "resource": {
-                    "id": `${myplaylistId}`,
-                    "snippet": {
-                      "title": "Updated sample playlist created via API",
-                      "description": "This is the updated playlist description.",
-                      "tags": [
-                        "updated playlist",
-                        "API FTW"
-                      ]
-                    },
-                    "status": {
-                      "privacyStatus": "private"
-                    }
-                  }
-                }).then(response => {
-                    console.log(response)
-                }).catch(err => {
-                    console.log('error')
-                })
-            }
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+                    
